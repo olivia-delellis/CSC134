341012b1e51002939c497802e0516cc311a2387b
olivia-delellis <delellio@faytechcc.edu>
2025-12-01 18:05:14 +0000

    deleted line

--- M6T1.cpp
+++ M6T1.cpp
@@ -45,3 +45,43 @@ void method1(){
     cout << "Total cars = " << car_total << endl;
     cout << "Average per day = " << car_avg << endl;
 }
+
+void method2() {
+    cout << "\nPart 2: Car Count (Arrays + Graph)\n";
+
+    const int SIZE = 5;
+    string days[SIZE] = {"Mon", "Tue", "Wed", "Thu", "Fri"};
+    int cars[SIZE];
+    int car_total = 0;
+    double car_avg = 0.0;
+
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Cars on " << days[i] << ": ";
+        cin >> cars[i];
+    }
+
+    cout << "\nDay\tCars" << endl;
+
+    for (int i = 0; i < SIZE; i++) {
+        cout << days[i] << "\t" << cars[i] << endl;
+        car_total += cars[i];
+    }
+
+    car_avg = (double) car_total / SIZE;
+
+    cout << "\nTotal cars = " << car_total << endl;
+    cout << "Average per day = " << car_avg << endl;
+
+
+    cout << "\nASCII Graph (1 star = 10 cars)" << endl;
+
+    for (int i = 0; i < SIZE; i++) {
+        cout << days[i] << " | ";
+
+        int stars = cars[i] / 10;  // scale so chart isn't huge
+        for (int s = 0; s < stars; s++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
